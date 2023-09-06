a6ca6b5eda90b166d9c33b229c08f3d05197dbd5
shantanuYadav1001 <shantanuyadav13@gmail.com>
2023-09-06 11:18:04 +0530

    removed_errors

--- main/Main.java
+++ main/Main.java
@@ -8,34 +8,39 @@ public class Main {
         Service service = new Service();
 
         // Task 1: Create a user, add user to roles, and print user details
-        Set<Integer> roleIds = new HashSet<>(Arrays.asList(1, 2));
-        service.createUser(1, "John", "Doe", "john@example.com", "password123", roleIds);
+        Set<Integer> roleIdsUser1 = new HashSet<>(Arrays.asList(1, 2));
+        service.createUser(1, "John", "Doe", "john@example.com", "password123", roleIdsUser1);
         
-        Employee user = service.getEmployeeById(1);
-//        if (user != null) {
-//            System.out.println("User ID: " + user.getId());
-//            System.out.println("First Name: " + user.getFirstName());
-//            System.out.println("Last Name: " + user.getLastName());
-//            System.out.println("Email: " + user.getEmail());
-//            System.out.println("Roles: " + user.getRoleIds());
-//        } else {
-//            System.out.println("User not found.");
-//        }
-        System.out.println("-----------------------------------");
+        Set<Integer> roleIdsUser2 = new HashSet<>(Arrays.asList(1, 3));
+        service.createUser(2, "Jack", "Smith", "jack@example.com", "password321", roleIdsUser2);
+        
         // Task 2: Add user to a role
-        service.addUserToRole(1, 3);
-        Employee e1 = service.getEmployeeById(1);
-//        System.out.println("User ID: " + user.getId());
-//        System.out.println("First Name: " + user.getFirstName());
-//        System.out.println("Last Name: " + user.getLastName());
-//        System.out.println("Email: " + user.getEmail());
-//        System.out.println("Roles: " + user.getRoleIds());
+        service.addUserToRole(1, 4);
+        
+        Employee t1 = service.getEmployeeById(1);
+        System.out.println("User ID: " + t1.getId());
+        System.out.println("First Name: " + t1.getFirstName());
+        System.out.println("Last Name: " + t1.getLastName());
+        System.out.println("Email: " + t1.getEmail());
+        System.out.println("Roles: " + t1.getRoleIds());
+        System.out.println("-----------------------------------");
+        
         
         // Task 3: Remove user from a role
-        //service.removeUserFromRole(1, 2);
-
+        service.removeUserFromRole(1, 2);
+        Employee t2 = service.getEmployeeById(1);
+        System.out.println("User ID: " + t2.getId());
+        System.out.println("First Name: " + t2.getFirstName());
+        System.out.println("Last Name: " + t2.getLastName());
+        System.out.println("Email: " + t2.getEmail());
+        System.out.println("Roles: " + t2.getRoleIds());
+        System.out.println("-----------------------------------");
+        
         // Task 4: Print list of users having both reader and creator roles
-        //service.getUsersWithRoles(1, 2);
+        service.addUserToRole(2, 4);
+        service.getUsersWithRoles(1, 4);
+        
+        Employee t3 = service.getEmployeeById(1);
         
         
      
--- main/Service.java
+++ main/Service.java
@@ -4,19 +4,53 @@ import java.util.List;
 import java.util.Set;
 
 class Service {
-    private List<Role> roles;
-    private List<Employee> emp = new ArrayList<>();
+	
+	
+    private List<Role> roles;			//create role list to save the Role hash set
+    private List<Employee> users;		//create user list to save the pointer to each employee object
+    
     public Service() {
+    	
+    	//initialize both lists
         roles = new ArrayList<>();
+        users = new ArrayList<>();
+        
         // Initialize roles in the constructor
         roles.add(new Role(1, "Reader", "Read-only access", new HashSet<>()));
         roles.add(new Role(2, "Creator", "Create content", new HashSet<>()));
         roles.add(new Role(3, "Editor", "Edit content", new HashSet<>()));
         roles.add(new Role(4, "Owner", "Full access", new HashSet<>()));
     }
+    
+    //this method returns the object reference in role list 
+    private Role getRoleById(int roleId) {
+        for (Role role : roles) {
+            if (role.getId() == roleId) {
+                return role;
+            }
+        }
+        return null;
+    }
+    
+    //this method return the object reference in users list
+    public Employee getEmployeeById(int userId) {
+        for(Employee employee : users) {
+        	if (employee.getId() == userId) {
+                return employee;
+        	}
+        }
+        return null;
+    }
+    
 
-    public void createUser(int id, String firstName, String lastName, String email, String password, Set<Integer> roleIds) {
+    //------------------------------------------------------------------------------------------------------
+    //method to create user
+    public void createUser(int id, String firstName, String lastName, String email, 
+    						String password, Set<Integer> roleIds) 
+    {	
+    	//create employee object and pass the values
         Employee employee = new Employee(id, firstName, lastName, email, password, roleIds);
+        
         
         for (int roleId : roleIds) {
             Role role = getRoleById(roleId);
@@ -24,7 +58,8 @@ class Service {
                 role.getEmployeeIds().add(id);
             }
         }
-        emp.add(employee);
+        //add the reference to the object in users list
+        users.add(employee);
         
         // Print user details
         System.out.println("User created: " + employee.getFirstName() + " " + employee.getLastName());
@@ -34,35 +69,53 @@ class Service {
         System.out.println("-----------------------------------");
     }
 
+    //---------------------------------------------------------------------------------------------------
+    //add user to role
     public void addUserToRole(int userId, int roleId) {
-    	if (roleId < 5 && roleId > 0) {
-	    	for(Employee e : emp) {
-	        	if (e.getId() == userId) {
-	        		e.getRoleIds().add(roleId);
-	                
-	        	}
-	        }
+    	
+    	Role role = getRoleById(roleId);
+    	Employee user = getEmployeeById(userId);
+    	
+    	if (role != null && user != null) {
+    		user.getRoleIds().add(roleId);
+    		role.getEmployeeIds().add(userId);
+    		System.out.println("Role with ID " + roleId + " was added to employee with ID" + userId);
     	}
-    	else {
-            System.out.println("Role with ID " + roleId + "not Found!!!");
+    	else if (role == null ) {
+            System.out.println("Role with ID " + roleId + " not Found!!!");
         }
+    	else if (user == null) {
+    		System.out.println("Employee with ID " + userId + " not Found!!!");
+    	}
        
     }
 
+    //remove user from role
     public void removeUserFromRole(int userId, int roleId) {
-        Role role = getRoleById(roleId);
-        if (role != null) {
-            boolean removed = role.getEmployeeIds().remove(userId);
-            if (removed) {
+        
+    	Role role = getRoleById(roleId);
+        Employee user = getEmployeeById(userId);
+        
+        if (role != null && user != null) {
+        	
+            boolean removeUserFromRole = role.getEmployeeIds().remove(userId);
+            boolean removeRoleFromUser = user.getRoleIds().remove(roleId);
+            
+            if (removeUserFromRole && removeRoleFromUser) {
                 System.out.println("User with ID " + userId + " removed from role " + role.getName());
             } else {
                 System.out.println("User with ID " + userId + " was not found in role " + role.getName());
             }
-        } else {
-            System.out.println("Role with ID " + roleId + " not found.");
+        } 
+        else if (role == null ) {
+            System.out.println("Role with ID " + roleId + "not Found!!!");
         }
+    	else if (user == null) {
+    		System.out.println("Employee with ID " + userId + "not Found!!!");
+    	}
     }
 
+    //get the all User Ids with these roles
     public void getUsersWithRoles(int roleId1, int roleId2) {
         Role role1 = getRoleById(roleId1);
         Role role2 = getRoleById(roleId2);
@@ -77,27 +130,7 @@ class Service {
         }
     }
 
-    private Role getRoleById(int roleId) {
-        for (Role role : roles) {
-            if (role.getId() == roleId) {
-                return role;
-            }
-        }
-        return null;
-    }
+   
     
-    public Employee getEmployeeById(int userId) {
-        for(Employee e : emp) {
-        	if (e.getId() == userId) {
-        		System.out.println("User name: " + e.getFirstName() + " " + e.getLastName());
-                System.out.println("User ID: " + e.getId());
-                System.out.println("Email: " + e.getEmail());
-                System.out.println("Roles: " + e.getRoleIds());
-                System.out.println("-----------------------------------");
-                
-        	}
-        }
-        // Employee with the specified userId not found
-        return null;
-    }
+    
 }
